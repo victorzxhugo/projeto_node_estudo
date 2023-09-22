c98b299ef15ad30b0bff30a92bab51c6c01a8ee3
victorzxhugo <victorzxhugoo@gmail.com>
2023-09-22 09:37:30 -0300

    servidor rodando, com arquivos html

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const http = require('http')
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router(); 
 
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!')
-});
-
-server.listen(port, hostname, () => {
-    console.log('Servidor rodando!')
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
+
+router.get('/sobre', function(req, res){
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+})
+
+
+app.use('/', router);
+
+app.listen(process.env.port || 3000);
+
+console.log('Server rodando')
